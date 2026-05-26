67b1fadc466189935dd1c72c9d64f1c518e5b51c
zelloronzipoapps-ctrl <zelloronzipoapps@gmail.com>
2026-05-26 09:38:42 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,35 +9,16 @@ const ASSETS = [
   './finallogo.jpg'
 ];
 
-// Install Service Worker
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activate Service Worker
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch Assets
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
+});
+
 });
